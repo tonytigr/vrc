35df8a4a2190d642bcd9a881d07d52a7ae9cc122
tonytigr <59077510+tonytigr@users.noreply.github.com>
2019-12-29 23:41:45 -0500

    tested

--- xDrive/include/robot-config.h
+++ xDrive/include/robot-config.h
@@ -10,14 +10,24 @@ extern motor RBMotor;
 extern controller Controller1;
 extern gyro TurnGyroSmart;
 
-#define maxMotorSpeed 	50
-#define numberOfMotors 	4
+#define MAX_MOTOR_SPEED 	50
 
-#define	WHEELS_WIDTH	46
 #define	WHEEL_CIRCUMFERENCE	31.919
 
 #define	PI	3.14159265358979
+//Range joystick not responds
 #define DEADBAND 20
+
+//accepted distance error in degree
+#define ACCEPTED_DISTANCE_ERROR 5
+//slow down distance in degree
+#define SLOW_DOWN_DISTANCE 120
+
+//accepted rotation error in degree
+#define ACCEPTED_ROTATION_ERROR 1
+//slow down rotation in degree
+#define SLOW_DOWN_ROTATION_DEGREE 25
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
  * 
@@ -29,6 +39,6 @@ double getJoyPolarRadians() ;
 double getJoyPolarSpeed();
 void RadianOutput(double radians, double speed, int rotation);
 void moveCordinate(double x1, double y1, double speedPercent);
-void moveCordinateWithPIDandGyro(double x1, double y1, int maxTurnPower, int minTurnPower, float kp);
+void moveCordinateGyro(double x1, double y1, int maxTurnPower, int minTurnPower, float kp);
 
 void TurnWithGyro(int targetDegrees, int maxTurnPower, int minTurnPower, float kp); 
