d580f1403953c146d6f5ac37844b4f05ef5f846d
tonytigr <59077510+tonytigr@users.noreply.github.com>
2020-01-02 11:13:53 -0500

    reorg files; tested blue_square() red_square()

--- andy/include/robot-config.h
+++ andy/include/robot-config.h
@@ -13,6 +13,17 @@ extern motor LeftIntake;
 extern motor RightIntake;
 extern motor_group Intake;
 
+extern int arm_level;
+extern int arm_level_previous;
+//arm position 0 - ground 1- elevated 2 - lower tower 3 - middle tower level
+extern int arm_position[4] ;
+
+extern int Tray_Velocity ;
+extern int tray_max_degrees ;
+extern int tray_position[4] ;
+
+extern int idle_flag ;
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
  * 
@@ -20,3 +31,8 @@ extern motor_group Intake;
  */
 void  vexcodeInit( void );
 void run_auto_15();
+void run_auto_60();
+void blue_rectangle();
+void blue_square();
+void red_rectangle();
+void red_square();
